cc1c5bd973911747260e272a525d452ed7620033
Rohan Marya <rohanmarya1507@gmail.com>
2024-06-08 00:06:38 +0530

    Update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,4 @@
-const { register, login, setAvatar, getAllUsers } = require("../controllers/userscontroller");
+const { register, login, setAvatar, getAllUsers } = require("../controllers/usersController");
 
 const router = require("express").Router();
 router.post("/register",register);
